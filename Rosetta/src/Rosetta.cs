b72b20f031c32a3bb33e1b9b0deff260bd738e20
VoidBehemoth <VoidBehemoth@gmail.com>
2023-11-08 10:50:38 -0700

    Add project files.

---
+++ Rosetta/src/Rosetta.cs
@@ -0,0 +1,23 @@
+﻿using SML;
+
+namespace Rosetta
+{
+    [Mod.SalemMod]
+    public class Main
+    {
+        public static void Start()
+        {
+            Rosetta.Utils.Logger.Log(ModInfo.PLUGIN_NAME + " has loaded!");
+        }
+    }
+
+
+    public static class ModInfo
+    {
+        public const string PLUGIN_GUID = "Rosetta";
+
+        public const string PLUGIN_NAME = "Rosetta";
+
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
---
+++ Rosetta/src/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Rosetta.Utils
+{
+    // Not necessary for mod, just useful
+    public class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine("[" + Rosetta.ModInfo.PLUGIN_GUID + "] " + message); // Formatting is a style choice on the developer's part
+        }
+    }
+}
